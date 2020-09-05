6ec9b54394fd5a366f9793ac07e8e8073eb3f99a
Forkbabu <sayantandas30011998@gmail.com>
2020-09-05 21:51:44 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -39,7 +39,7 @@ def load_checkpoints(config_path, checkpoint_path, cpu=False):
     if cpu:
         checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
     else:
-        checkpoint = torch.load(checkpoint_path)
+        checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
  
     generator.load_state_dict(checkpoint['generator'])
     kp_detector.load_state_dict(checkpoint['kp_detector'])
